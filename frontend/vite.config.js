8ccd79f59f7f5bdf0ca33f8f51ed3a7513992ddd
kotbarsik96 <kotbarsik96@gmail.com>
2023-09-25 16:54:14 +0400

    добавление frontend в docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    server: {
+        watch: {
+            usePolling: true
+        },
+        host: true,
+        strictPort: true,
+        port: 5173
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
